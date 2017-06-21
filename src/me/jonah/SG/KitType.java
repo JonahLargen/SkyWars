7644ec95d9c5fdad43358bc27e74f42c9e231aaa
Jonah <unconfigured@null.spigotmc.org>
2017-06-21 16:50:49 -0400

    Master

---
+++ src/me/jonah/SG/KitType.java
@@ -0,0 +1,17 @@
+package me.jonah.SG;
+
+public enum KitType {
+	
+	APPLER,BOXER,
+	
+	RUGGED,RANGER,
+	
+	CANNIBAL, ESCAPIST, TRACKER, ENCHANTER,
+	
+	ANGEL, BRUTE, SHARPSHOOTER, SAVAGE
+	
+	
+	
+	
+
+}
